5596bcb03cb2b9d6bd1be57b72ef7da26e184748
Sergey Samokhvalov <darklordddddd@gmail.com>
2017-02-27 12:39:05 -0800

    init/uninit

--- pipe.c
+++ pipe.c
@@ -2,51 +2,42 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/device.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
 #include <linux/fs.h>
-#include <linux/slab.h>
 
 #define DEVICE_NAME "my_pipe"
-#define CLASS_NAME "pipe"
 
-static int buf_size;
+static int buf_size = 1;
 static int *buf;
-static int major;
-static struct class* pipeClass  = NULL;
-static struct device* pipeDevice = NULL; 
+static int pipe_major;
 module_param(buf_size, int, 0);
 
+static dev_t pipe_dev;
+
+/*static struct file_operations fops = 
+{
+	.open = pipe_open,
+	.read = pipe_read,
+	.write = pipe_write,
+	.release = pipe_release,
+};*/
+
 static int __init char_device_init(void) {
+	
+	int ret;
+	
 	// Dynamic major number
-	majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
-	if (majorNumber < 0) {
-		printk(KERN_ALERT "My_pipe: Failed to register a major number\n");
+	ret = alloc_chrdev_region(&pipe_dev, 0, 1, DEVICE_NAME);
+	if (ret < 0) {
+		printk(KERN_ALERT "My_pipe: Failed to get a major number\n");
 		return -1;
 	}
-	printk(KERN_INFO "My_pipe: Registered correctly with major number %d\n", majorNumber);
- 
-	// Register the device class
-	pipeClass = class_create(THIS_MODULE, CLASS_NAME);
-	if (IS_ERR(pipeClass)) {
-		unregister_chrdev(majorNumber, DEVICE_NAME);
-		printk(KERN_ALERT "My_pipe: Failed to register device class\n");
-		return PTR_ERR(pipeClass);
-	}
-	printk(KERN_INFO "My_pipe: Device class registered correctly\n");
- 
-	// Register the device driver
-	pipeDevice = device_create(pipeClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
-	if (IS_ERR(pipeDevice)) {
-		class_destroy(pipeClass);
-		unregister_chrdev(majorNumber, DEVICE_NAME);
-		printk(KERN_ALERT "My_pipe: Failed to create the device\n");
-		return PTR_ERR(pipeDevice);
-	}
-	printk(KERN_INFO "My_pipe: device class created correctly\n");
+	printk(KERN_INFO "My_pipe: Registered correctly with major: %d and minor: %d number %d\n", MAJOR(pipe_dev), MINOR(pipe_dev));
 
 	// Check buffer size
 	printk(KERN_INFO "My_pipe: Pipe init\n");
-	if (buf_size < 0) {
+	if (buf_size <= 0) {
 		printk(KERN_ALERT "My_pipe: Wrong buffer size!\n");
 		return -1;	
 	}
@@ -58,10 +49,7 @@ static int __init char_device_init(void) {
 }
 
 static void __exit char_device_exit(void) {
-	device_destroy(pipeClass, MKDEV(majorNumber, 0));
-	class_unregister(pipeClass);
-	class_destroy(pipeClass);
-	unregister_chrdev(majorNumber, DEVICE_NAME);
+	unregister_chrdev_region(pipe_dev, 1);
 	printk(KERN_INFO "My_pipe: uninit completed\n");
 }
 
