d50f7d592c12602f8af82d0e872129ed7cb66f2d
Sergey Samokhvalov <darklordddddd@gmail.com>
2017-03-13 03:34:56 -0700

    pipe+fops+cat
    Signed-off-by: Sergey Samokhvalov <darklordddddd@gmail.com>

--- pipe.c
+++ pipe.c
@@ -12,6 +12,7 @@
 #include <asm/uaccess.h>
 
 #define DEVICE_NAME "my_pipe"
+#define SU_ID 0
 
 /* связанный список указателей на буферы
  * для каждого пользователя
@@ -32,7 +33,6 @@ static struct user_buf uB;
 
 //число пользователей
 static int count;
-module_param(count, int, 0000);
 
 static int buf_size = 1;
 module_param(buf_size, int, 0000);
@@ -40,7 +40,36 @@ module_param(buf_size, int, 0000);
 static dev_t pipe_dev;
 static struct cdev my_cdev;
 
-int pipe_open(struct inode *i, struct file *f)
+//func
+static int pipe_open(struct inode *i, struct file *f);
+static int pipe_release(struct inode *i, struct file *f);
+static ssize_t pipe_read(struct file *f, char __user *bf,
+size_t sz, loff_t *off);
+static ssize_t pipe_write(struct file *f, const char __user *bf,
+size_t sz, loff_t *off);
+static int su_pipe_release(struct inode *i, struct file *f);
+static ssize_t su_pipe_read(struct file *f, char __user *bf,
+size_t sz, loff_t *off);
+static ssize_t su_pipe_write(struct file *f, const char __user *bf,
+size_t sz, loff_t *off);
+
+static const struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.open = pipe_open,
+	.read = pipe_read,
+	.write = pipe_write,
+	.release = pipe_release,
+};
+
+static const struct file_operations su_fops = {
+	.owner = THIS_MODULE,
+	.open = pipe_open,
+	.read = su_pipe_read,
+	.write = su_pipe_write,
+	.release = su_pipe_release,
+};
+
+static int pipe_open(struct inode *i, struct file *f)
 {
 	/* читаем пользователя и проверяем,
 	 * есть ли он в базе
@@ -55,7 +84,12 @@ int pipe_open(struct inode *i, struct file *f)
 		 * что уже открыто
 		 */
 		if (u->user_id.val == id.val) {
-			pr_info("My_pipe: device has been opened for this user\n");
+			//pr_info("id = %d\n", id.val);
+			if (id.val == SU_ID) {
+				pr_info("My_pipe: device has been opened for superuser\n");
+				f->f_op = &su_fops;
+			} else
+				pr_info("My_pipe: device has been opened for this user\n");
 			return 0;
 		}
 	}
@@ -72,17 +106,60 @@ int pipe_open(struct inode *i, struct file *f)
 	INIT_LIST_HEAD(&u->list);
 	list_add(&u->list, &uB.list);
 	count++;
-	pr_info("My_pipe: device has been opened for this user\n");
+	//pr_info("id = %d\n", id.val);
+	if (id.val == SU_ID) {
+		pr_info("Superuser open\n");
+		f->f_op = &su_fops;
+	} else
+		pr_info("My_pipe: device has been opened for this user\n");
 	return 0;
 }
 
-int pipe_release(struct inode *i, struct file *f)
+static int pipe_release(struct inode *i, struct file *f)
 {
+	kuid_t id = current_uid();
+	struct user_buf *u;
+
+	// writer
+	if (f->f_mode & FMODE_WRITE) {
+		if (down_interruptible(&sem))
+		return -ERESTARTSYS;
+
+		list_for_each_entry(u, &uB.list, list) {
+			if (u->user_id.val == id.val) {
+				//некуда писать
+				while ((u->w_index + 1) % buf_size ==
+				u->r_index) {
+					up(&sem);
+					//pr_info("Write sleep\n");
+					wait_event_interruptible(wq,
+					(u->w_index + 1) % buf_size
+					!= u->r_index);
+					if (down_interruptible(&sem))
+						return -ERESTARTSYS;
+					//return 0;
+				}
+				u->buf[u->w_index] = 0;
+				//pr_info("My_pipe: write %c\n",
+					//u->buf[u->w_index]);
+				u->w_index++;
+				if (u->w_index == buf_size)
+					u->w_index = 0;
+				//pr_info("w_index = %ld\n", u->w_index);
+				//pr_info("r_index = %ld\n", u->r_index);
+
+				goto w_rdy;
+			}
+		}
+	}
+w_rdy:
+	up(&sem);
+	wake_up_interruptible(&rq);
 	pr_info("My_pipe: device closed for this user\n");
 	return 0;
 }
 
-ssize_t pipe_read(struct file *f, char __user *bf,
+static ssize_t pipe_read(struct file *f, char __user *bf,
 size_t sz, loff_t *off) {
 	/* читаем пользователя
 	 * и читаем только из его буфера
@@ -101,14 +178,20 @@ size_t sz, loff_t *off) {
 				up(&sem);
 				//pr_info("Read sleep\n");
 				if (wait_event_interruptible(rq,
-				u->r_index != u->w_index)) {
+				(u->r_index != u->w_index))) {
 				// если было прерывание
 					return -ERESTARTSYS;
 				}
 				if (down_interruptible(&sem))
 					return -ERESTARTSYS;
 			}
-
+			if (u->buf[u->r_index] == 0) {
+				//pr_info("EOF\n");
+				u->r_index++;
+				if (u->r_index == buf_size)
+					u->r_index = 0;
+				return 0;
+			}
 			copy_to_user(bf, &u->buf[u->r_index], 1);
 			//pr_info("My_pipe: read %c\n", u->buf[u->r_index]);
 			u->r_index++;
@@ -126,7 +209,7 @@ r_rdy:
 	return 1;
 }
 
-ssize_t pipe_write(struct file *f, const char __user *bf,
+static ssize_t pipe_write(struct file *f, const char __user *bf,
 size_t sz, loff_t *off) {
 	/* читаем пользователя
 	 * и пишем именно в его буфер
@@ -168,17 +251,142 @@ w_rdy:
 	return 1;
 }
 
-static const struct file_operations fops = {
-	.owner = THIS_MODULE,
-	.open = pipe_open,
-	.read = pipe_read,
-	.write = pipe_write,
-	.release = pipe_release,
-};
+//su file operations
+
+static int su_pipe_release(struct inode *i, struct file *f)
+{
+	struct user_buf *u;
+
+	// writer
+	if (f->f_mode & FMODE_WRITE) {
+		if (down_interruptible(&sem))
+		return -ERESTARTSYS;
+
+		list_for_each_entry(u, &uB.list, list) {
+			if (u->user_id.val == SU_ID) {
+				//некуда писать
+				while ((u->w_index + 1) % buf_size ==
+				u->r_index) {
+					up(&sem);
+					//pr_info("Write sleep\n");
+					wait_event_interruptible(wq,
+					(u->w_index + 1) % buf_size !=
+					u->r_index);
+					if (down_interruptible(&sem))
+						return -ERESTARTSYS;
+					//return 0;
+				}
+				u->buf[u->w_index] = 0;
+				//pr_info("My_pipe: write %c\n",
+					//u->buf[u->w_index]);
+				u->w_index++;
+				if (u->w_index == buf_size)
+					u->w_index = 0;
+				//pr_info("w_index = %ld\n", u->w_index);
+				//pr_info("r_index = %ld\n", u->r_index);
+
+
+				goto w_rdy;
+			}
+		}
+	}
+w_rdy:
+	up(&sem);
+	wake_up_interruptible(&rq);
+	pr_info("My_pipe: device closed for superuser\n");
+	return 0;
+}
+
+static ssize_t su_pipe_read(struct file *f, char __user *bf,
+size_t sz, loff_t *off) {
+	/* читаем пользователя
+	 * и читаем только из его буфера
+	 */
+	struct user_buf *u;
+
+	if (down_interruptible(&sem))
+		return -ERESTARTSYS;
+
+	list_for_each_entry(u, &uB.list, list) {
+		if (u->user_id.val == SU_ID) {
+			// нечего читать
+			while (u->r_index == u->w_index) {
+				up(&sem);
+				//pr_info("Read sleep\n");
+				if (wait_event_interruptible(rq,
+				(u->r_index != u->w_index))) {
+				// если было прерывание
+					return -ERESTARTSYS;
+				}
+				if (down_interruptible(&sem))
+					return -ERESTARTSYS;
+			}
+			if (u->buf[u->r_index] == 0) {
+				//pr_info("EOF\n");
+				u->r_index++;
+				if (u->r_index == buf_size)
+					u->r_index = 0;
+				return 0;
+			}
+			copy_to_user(bf, &u->buf[u->r_index], 1);
+			//pr_info("My_pipe: read %c\n", u->buf[u->r_index]);
+			u->r_index++;
+			if (u->r_index == buf_size)
+				u->r_index = 0;
+			//pr_info("w_index = %ld\n", u->w_index);
+			//pr_info("r_index = %ld\n", u->r_index);
+
+			goto r_rdy;
+		}
+	}
+r_rdy:
+	up(&sem);
+	wake_up_interruptible(&wq);
+	return 1;
+}
+
+static ssize_t su_pipe_write(struct file *f, const char __user *bf,
+size_t sz, loff_t *off) {
+	/* читаем пользователя
+	 * и пишем именно в его буфер
+	 */
+	struct user_buf *u;
+
+	if (down_interruptible(&sem))
+		return -ERESTARTSYS;
+
+	list_for_each_entry(u, &uB.list, list) {
+		if (u->user_id.val == SU_ID) {
+			//некуда писать
+			while ((u->w_index + 1) % buf_size == u->r_index) {
+				up(&sem);
+				//pr_info("Write sleep\n");
+				wait_event_interruptible(wq,
+				(u->w_index + 1) % buf_size != u->r_index);
+				if (down_interruptible(&sem))
+					return -ERESTARTSYS;
+				//return 0;
+			}
+			copy_from_user(&u->buf[u->w_index], bf, 1);
+			//pr_info("My_pipe: write %c\n",
+				//u->buf[u->w_index]);
+			u->w_index++;
+			if (u->w_index == buf_size)
+				u->w_index = 0;
+			//pr_info("w_index = %ld\n", u->w_index);
+			//pr_info("r_index = %ld\n", u->r_index);
+
+			goto w_rdy;
+		}
+	}
+w_rdy:
+	up(&sem);
+	wake_up_interruptible(&rq);
+	return 1;
+}
 
 static int __init char_device_init(void)
 {
-
 	int ret;
 
 	// Dynamic major number
